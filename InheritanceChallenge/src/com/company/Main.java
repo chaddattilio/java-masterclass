ede7f6b546504b8060e1942cb598f47aa10475be
Chad Dattilio <cdattilio@lyonscg.com>
2019-11-07 19:45:58 -0500

    More inheritance practice

---
+++ InheritanceChallenge/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Edge edge = new Edge(36);
+        edge.steer(45);
+	    edge.accelerate(20);
+    }
+}
